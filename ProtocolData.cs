33b1d406e3ed0f22d01de18be27c17b4c3adb28b
reco2008 <reco200814@gmail.com>
2025-01-16 12:18:03 +0300

    Create ProtocolData.cs

---
+++ ProtocolData.cs
@@ -0,0 +1,9 @@
+namespace SmartTrackingAI.Models
+{
+    public class ProtocolData
+    {
+        public int Id { get; set; }
+        public string FieldName { get; set; }
+        public string FieldValue { get; set; }
+    }
+}
